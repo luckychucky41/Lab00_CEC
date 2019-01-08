e3ea45c1f389f7fbc3af6404d50c116b9b1eb9d0
Charlie Cadell <ccadell@bellarmine.edu>
2019-01-08 10:24:50 -0500

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,45 @@
+
+public class Circle {
+	private double radius;//instance variable/field
+	
+	public Circle() {
+		setRadius(1.0);
+		
+	}//end empty-argument constructor
+
+
+	public Circle(double r) {
+		
+		setRadius(r);
+	}//end preferred constructor
+
+	public double calculateArea() {
+		
+		double area= Math.PI * Math.pow(radius, 2.0);
+		return area;
+		
+	}//end calculateArea
+	
+	
+	public double getRadius() {
+		return radius;
+	}//end getRadiuse
+
+
+	public void setRadius(double radius) {
+		
+		if(radius<1.0)
+			this.radius=1.0;
+		else
+			this.radius = radius;
+	}//end setRadius
+
+
+	@Override
+	public String toString() {
+		return "Circle [radius=" + radius + "]";
+	}//end toString
+	
+	
+	
+}//end class
